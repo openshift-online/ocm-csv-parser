ac20b4d4013677c8b1dc9cfc0b70025be86f4b79
hkepley <hkepley@redhat.com>
2023-11-10 10:30:50 -0500

    Validations for datatypes + tests + cleanup of tests + etc

---
+++ pkg/fieldnames/field_names.go
@@ -0,0 +1,16 @@
+package fieldnames
+
+const (
+	Id              = "id"
+	GenericName     = "generic_name"
+	Name            = "name"
+	CloudProviderId = "cloud_provider_id"
+	CpuCores        = "cpu_cores"
+	Memory          = "memory"
+	MemoryPretty    = "memory_pretty"
+	Category        = "category"
+	CategoryPretty  = "category_pretty"
+	SizePretty      = "size_pretty"
+	ResourceType    = "resource_type"
+	Active          = "active"
+)
